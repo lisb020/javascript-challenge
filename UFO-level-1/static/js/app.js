ed70838639d1f168d01d1649e08ba895ab922643
Lisa Stroh <lisastroh@Lisas-MacBook-Air.local>
2021-02-06 20:59:35 -0800

    setting up table and filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,7 @@
 // from data.js
 let tableData = data;
+let dateVar = "1/12/2010";
 
-// YOUR CODE HERE!
+// filter for date
+dateTable = tableData.filter(dt => dt.datetime === dateVar);
+console.log(dateTable);
